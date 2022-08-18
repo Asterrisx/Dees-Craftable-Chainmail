1093ed693d4275dde1bcd67ad3ddcd1721a7adf2
DeesasterComposer <boondinisdipperis@gmail.com>
2022-08-18 01:20:58 -0500

    Ported mod to 1.16.5

--- src/main/java/net/deesaster/deescraftablechainmail/DeesCraftableChainmail.java
+++ src/main/java/net/deesaster/deescraftablechainmail/DeesCraftableChainmail.java
@@ -2,18 +2,13 @@ package net.deesaster.deescraftablechainmail;
 
 import net.deesaster.deescraftablechainmail.item.ModItems;
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class DeesCraftableChainmail implements ModInitializer {
 	public static final String MOD_ID = "deescraftablechainmail";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 
 		ModItems.registerModItems();
-
-		LOGGER.info("Chainmail is now craftable");
 	}
 }
