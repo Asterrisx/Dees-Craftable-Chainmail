9c61ed3d09cdcbaf913eb5bb8fc7aebb83df4aa3
DeesasterComposer <boondinisdipperis@gmail.com>
2022-08-18 00:42:43 -0500

    Initial commit

---
+++ src/main/java/net/deesaster/deescraftablechainmail/DeesCraftableChainmailClient.java
@@ -0,0 +1,9 @@
+package net.deesaster.deescraftablechainmail;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeesCraftableChainmailClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
